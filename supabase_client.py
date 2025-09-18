064bcd2958052f2875ac750f5d0c3b9ce0c906e9
Jackson Rwegasira <rwegasirajackson11@gmail.com>
2025-09-18 03:30:33 +0300

    FIX ENV VARIABLES FOR SUPABASE AND SECURE KEYS

--- supabase_client.py
+++ supabase_client.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
-SUPABASE_URL = os.getenv("https://glpkpcekdilmffzsvmzg.supabase.co")
-SUPABASE_KEY = os.getenv("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdscGtwY2VrZGlsbWZmenN2bXpnIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1MjYyMDgyNCwiZXhwIjoyMDY4MTk2ODI0fQ.w07mhBysg_8AbX7m3-CcSASDaGShaf1hQb6Lwc21BF4")
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
 if not SUPABASE_URL or not SUPABASE_KEY:
     raise ValueError("Missing Supabase environment variables!")
